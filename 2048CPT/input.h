82a45ef4e1b207761a60bb0b259ec42571a1caff
Hugo Xia <hugo.h.xia@gmail.com>
2023-10-25 14:02:38 +0200

    Input.h Pushed

---
+++ 2048CPT/input.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <conio.h>
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
+void keyDetect()
+{
+    bool badKey = true;
+    while (badKey)
+    {
+        badKey = false;
+        int c = 0;
+        switch ((c = _getch()))
+        {
+        case KEY_UP:
+            break;
+        case KEY_DOWN:
+            break;
+        case KEY_RIGHT:
+            break;
+        case KEY_LEFT:
+            break;
+        default:
+            badKey = true;
+            break;
+        }
+    }
+}
